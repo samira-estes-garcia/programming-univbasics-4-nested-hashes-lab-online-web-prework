d9b200b55e0d990fd940d39482732b9dbb145a09
Samira Estes-Garcia <samira.estes.garcia@gmail.com>
2019-09-03 15:39:22 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -115,7 +115,7 @@ def changing_alan
      alans_new_info = "GUI"
      
      programmer_hash[:alan_kay][:known_for] = alans_new_info
-     puts programmer_hash
+     return programmer_hash
      
 end
 
