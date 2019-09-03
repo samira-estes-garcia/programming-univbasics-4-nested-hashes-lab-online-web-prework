b509227232d9ecb750593df768f1197373b53419
Samira Estes-Garcia <samira.estes.garcia@gmail.com>
2019-09-03 15:30:48 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -88,7 +88,11 @@ def adding_matz
         }
      }
      
-  programmer_hash
+  programmer_hash[:yukihiro_matsumoto] => 
+  {
+   :known_for => "Ruby",
+   :languages => ["LISP", "C"]
+   }
      
 end
 
